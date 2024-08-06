0e0a94e371538e26e8fd62a7fcf71539c1a03129
Pohan <oange6214@gmail.com>
2024-08-07 01:01:37 +0800

    Add Fonts, Colors, Buttons Style. Add Titlebar close, minize, maxize and logic (code-behind)

--- src/FileExplorerRedesign/MainWindow.xaml.cs
+++ src/FileExplorerRedesign/MainWindow.xaml.cs
@@ -1,15 +1,7 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace FileExplorerRedesign;
+
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
@@ -18,5 +10,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void minimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void maximizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+        }
+        else
+        {
+            WindowState = WindowState.Maximized;
+        }
+    }
+
+    private void closeButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
