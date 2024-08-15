3ef68d3808783c7732a3ba942c3f914bef4b0a6d
Pohan <oange6214@gmail.com>
2024-08-16 07:49:18 +0800

    Add Window max/min base on window api. Add Menu trigger.

---
+++ src/FileExplorerRedesign/Converters/DpiAwarePaddingConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FileExplorerRedesign.Converters;
+
+public class DpiAwarePaddingConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length < 2 || !(values[0] is Window window) || !(values[1] is Thickness baseThickness))
+            return new Thickness(0);
+
+        var dpi = VisualTreeHelper.GetDpi(window);
+        return new Thickness(
+            baseThickness.Left / dpi.DpiScaleX,
+            baseThickness.Top / dpi.DpiScaleY,
+            baseThickness.Right / dpi.DpiScaleX,
+            baseThickness.Bottom / dpi.DpiScaleY
+        );
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/FileExplorerRedesign/Views/MainWindow.xaml.cs
+++ src/FileExplorerRedesign/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace FileExplorerRedesign.Views;
 
@@ -10,5 +12,82 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+        ((HwndSource)PresentationSource.FromVisual(this)).AddHook(HookProc);
+    }
+
+    private IntPtr HookProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+        if (msg == 0x0024) // WM_GETMINMAXINFO
+        {
+            WmGetMinMaxInfo(hwnd, lParam);
+            handled = true;
+        }
+        return (IntPtr)0;
+    }
+
+    private void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+    {
+        MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+        uint MONITOR_DEFAULTTONEAREST = 0x00000002;
+        IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+        if (monitor != IntPtr.Zero)
+        {
+            MONITORINFO monitorInfo = new MONITORINFO();
+            GetMonitorInfo(monitor, monitorInfo);
+            RECT rcWorkArea = monitorInfo.rcWork;
+            RECT rcMonitorArea = monitorInfo.rcMonitor;
+            mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+            mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+            mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+            mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+        }
+        Marshal.StructureToPtr(mmi, lParam, true);
+    }
+
+    // P/Invoke declarations
+    [DllImport("user32.dll")]
+    private static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+
+    [DllImport("user32.dll")]
+    private static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int x;
+        public int y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MINMAXINFO
+    {
+        public POINT ptReserved;
+        public POINT ptMaxSize;
+        public POINT ptMaxPosition;
+        public POINT ptMinTrackSize;
+        public POINT ptMaxTrackSize;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class MONITORINFO
+    {
+        public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+        public RECT rcMonitor = new RECT();
+        public RECT rcWork = new RECT();
+        public int dwFlags = 0;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
     }
 }
