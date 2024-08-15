a9c0bc9a45a30ed5a76bfc035cdf268196e119e3
Pohan <oange6214@gmail.com>
2024-08-16 00:14:23 +0800

    Fix style, and split all style to control and resources.

---
+++ src/FileExplorerRedesign/Controls/DriveAndFolderButton/DriveAndFolderButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FileExplorerRedesign.Controls;
+
+public class DriveAndFolderButton : RadioButton
+{
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register(
+            nameof(Icon),
+            typeof(PathGeometry),
+            typeof(DriveAndFolderButton)
+        );
+
+    public PathGeometry Icon
+    {
+        get { return (PathGeometry)GetValue(IconProperty); }
+        set { SetValue(IconProperty, value); }
+    }
+}
--- src/FileExplorerRedesign/ViewModels/MainWindowViewModel.cs
+++ src/FileExplorerRedesign/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,51 @@
-﻿namespace FileExplorerRedesign.ViewModels;
+﻿using System.Windows;
+using Toolkit.Mvvm.ComponentModel;
+using Toolkit.Mvvm.Input;
 
-public class MainWindowViewModel
+namespace FileExplorerRedesign.ViewModels;
+
+public class MainWindowViewModel : ObservableObject
 {
+    private IRelayCommand _closeCommand;
+    private IRelayCommand _maximizeCommand;
+    private IRelayCommand _minimizeCommand;
+    private WindowState _windowState;
+
+    public MainWindowViewModel()
+    {
+    }
+
+    public IRelayCommand CloseCommand => _closeCommand ??= new RelayCommand(Close);
+
+    public IRelayCommand MaximizeCommand => _maximizeCommand ??= new RelayCommand(Maximize);
+
+    public IRelayCommand MinimizeCommand => _minimizeCommand ??= new RelayCommand(Minimize);
+
+    public WindowState WindowState
+    {
+        get => _windowState;
+        set => SetProperty(ref _windowState, value);
+    }
+
+    private void Close()
+    {
+        Application.Current.MainWindow.Close();
+    }
+
+    private void Maximize()
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+        }
+        else
+        {
+            WindowState = WindowState.Maximized;
+        }
+    }
+
+    private void Minimize()
+    {
+        WindowState = WindowState.Minimized;
+    }
 }
