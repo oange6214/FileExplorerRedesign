ec86e7b305369e3694109fab31839172070830ec
Pohan <oange6214@gmail.com>
2024-08-16 07:09:49 +0800

    Fix all style. Add Menul.

--- src/FileExplorerRedesign/App.xaml.cs
+++ src/FileExplorerRedesign/App.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace FileExplorerRedesign;
 
