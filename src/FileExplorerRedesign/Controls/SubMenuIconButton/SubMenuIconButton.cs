4cdb18f8ef896c81e520ccded42608eadd29d509
Pohan <oange6214@gmail.com>
2025-10-30 23:36:11 +0800

    chore: Add SubMenuIconButton.

---
+++ src/FileExplorerRedesign/Controls/SubMenuIconButton/SubMenuIconButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FileExplorerRedesign.Controls;
+
+public class SubMenuIconButton : Button
+{
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register(
+            nameof(Icon),
+            typeof(PathGeometry),
+            typeof(SubMenuIconButton)
+        );
+
+    public PathGeometry Icon
+    {
+        get => (PathGeometry)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}
